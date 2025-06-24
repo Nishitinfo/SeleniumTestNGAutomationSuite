7d6ac286e2feb8f0ec2430212b8fc186bcd5b096
nishitsheth13 <Nishit.Sheth@mrisoftware.com>
2025-06-24 18:07:08 +0530

    updated jira configs

--- src/main/java/basicTemplate/jira/jiraConfig.java
+++ src/main/java/basicTemplate/jira/jiraConfig.java
@@ -1,20 +1,11 @@
 package basicTemplate.jira;
+
+import basicTemplate.configs.loadProps;
+
 public class jiraConfig {
-    public static final String BASE_URL = "https://nishitshethinfo.atlassian.net";
-    public static final String EMAIL = "nishitshethinfo@gmail.com";
-    public static final String API_TOKEN = "ATATT3xFfGF075ydN4Ci8ED8cHPCp0BPCaGUpp18wHx_mJ5huU3daSjJP97vir37PevZ3d97gQXKC480CujDhqqbXv0dxMv0qSh0K3v2oLzeq9r50XiGmsQI9M8mREZtbPEdyZCQwzEXn9UNBydPfSwHtE5LVn6bU2BA1xe3_ShCBSxD1jYOzoE=5029EB28";
-    public static final String PROJECT_KEY = "SCRUM";
+    public static final String BASE_URL = loadProps.getProperty("Jira_BASE_URL");
+    public static final String EMAIL = loadProps.getProperty("Jira_Email");
+    public static final String API_TOKEN = loadProps.getProperty("Jira_API_Token");
+    public static final String PROJECT_KEY = loadProps.getProperty("Jira_PROJECT_KEY");
 }
 
-//Client id
-//33ADDA5757514A63B65A2A95B48B1855
-
-//Scerate
-//a0bb9ec518af046cfead37e3674f34592bbe7d1367748fabeb181267cb1bf88d
-
-
-//New
-//ATATT3xFfGF075ydN4Ci8ED8cHPCp0BPCaGUpp18wHx_mJ5huU3daSjJP97vir37PevZ3d97gQXKC480CujDhqqbXv0dxMv0qSh0K3v2oLzeq9r50XiGmsQI9M8mREZtbPEdyZCQwzEXn9UNBydPfSwHtE5LVn6bU2BA1xe3_ShCBSxD1jYOzoE=5029EB28
-
-//old
-//ATCTT3xFfGN0mZLE5usber4AzUlyEfWni4H4YlaHRZumeATgW_ze72G1fgvd0OKhGGJgtd3mIB0nGTototKcoT6UhxrXDXzkgV6Bhvsj66sV4FkCr-b2HxMKSMkfeEMJ9L7fCL9RHzwHk3DbOLT8sszXVWLNeiXEA_omo6n-qMp06a6JHYsvNp0=6102A80F
